e81a7e4f53e5195c774dfa61442fd9dfdaa903df
Alexandr <87350052+koveev@users.noreply.github.com>
2021-11-03 23:19:48 +0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -19,3 +19,6 @@ INSERT INTO Product (Product, CategoryId) VALUES ('Bread', 2)
 INSERT INTO Product (Product, CategoryId) VALUES ('Rice', 1)
 INSERT INTO Product (Product, CategoryId) VALUES ('Buckwheat', 1)
 INSERT INTO Product (Product) VALUES ('Pear')
+
+
+SELECT a.Product, b.Category FROM Product a LEFT JOIN Category b ON a.CategoryId = b.Id
